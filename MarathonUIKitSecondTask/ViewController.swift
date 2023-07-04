aa1033e60f12696b8ad0c5c02468c4ebbb1631fa
Stacy <filin.owls@gmail.com>
2023-07-04 10:28:22 +0300

    Initial Commit

---
+++ MarathonUIKitSecondTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarathonUIKitSecondTask
+//
+//  Created by Anastasiia Perminova on 04.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
